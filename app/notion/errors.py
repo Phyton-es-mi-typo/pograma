8f30aa46f1fb6b278decfe86035bf9c06605a1e0
Cristóbal Contreras Rubio <crisconru@gmail.com>
2021-03-14 12:25:56 +0100

    added errors

---
+++ app/notion/errors.py
@@ -0,0 +1,10 @@
+class NotionServiceException(Exception):
+    """Internal Exceptions"""
+
+    pass
+
+
+class NotionProviderException(Exception):
+    """Generic exceptions for weather provider"""
+
+    pass
---
+++ app/weather/errors.py
@@ -0,0 +1,10 @@
+class WeatherServiceException(Exception):
+    """Internal Exceptions"""
+
+    pass
+
+
+class WeatherProviderException(Exception):
+    """Generic exceptions for weather provider"""
+
+    pass
