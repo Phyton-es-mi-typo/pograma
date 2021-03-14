133bf5d2e296b892240494d968c5c56ded17b32f
Jose Rivera <josemrivera@gmail.com>
2021-03-14 22:45:45 +0000

    Start class design.

--- tests/weather/test_weather_service.py
+++ tests/weather/test_weather_service.py
@@ -1,2 +1,11 @@
-def test_weather_entity():
-    assert 1 == True
+from app.weather import entities
+
+
+def test_weather_conditions():
+    temperature = 8.3
+    pressure = 1002.71
+    precipitation = 0.25
+    weather_conditions = entities.WeatherConditions(
+        temperature=temperature, pressure=pressure, precipitation=precipitation
+    )
+    assert weather_conditions.temperature == temperature
